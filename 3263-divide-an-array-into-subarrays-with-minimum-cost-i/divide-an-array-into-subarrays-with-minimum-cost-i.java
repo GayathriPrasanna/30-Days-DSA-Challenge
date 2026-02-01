89253ef7e4ead5796e19399552a92221598ac014
Chilakalapalli GayathriPrasanna <gayathri162004@gmail.com>
2026-02-01 21:12:16 +0530

    Time: 1 ms (99.20%) | Memory: 45.5 MB (58.40%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,9 +1,33 @@
+// class Solution {
+//     public int minimumCost(int[] nums) {
+//         int first = nums[0];
+
+//         Arrays.sort(nums, 1, nums.length);
+
+//         return first + nums[1] + nums[2];
+//     }
+// }
+
 class Solution {
     public int minimumCost(int[] nums) {
+
         int first = nums[0];
 
-        Arrays.sort(nums, 1, nums.length);
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-        return first + nums[1] + nums[2];
+        // find two smallest elements after index 0
+        for(int i = 1; i < nums.length; i++){
+            
+            if(nums[i] < min1){
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
+
+        return first + min1 + min2;
     }
 }
